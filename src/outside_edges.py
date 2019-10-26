b69df4b4448851b11f3d462e632c3f371d7122c6
brightcat1 <bright11jp@yahoo.co.jp>
2019-10-26 17:22:20 +0900

    映像用と描画用のキャンバスを分割

--- src/outside_edges.py
+++ src/outside_edges.py
@@ -13,18 +13,18 @@ root.title("Oekaki Hack")
 root.geometry("1280x720")
 
 
-#メインウィンドウ全体をキャンバスにする
-canvas = tk.Canvas(root,width = 1280, height = 720)
+#カメラ映像用のキャンバス（左上）
+canvas = tk.Canvas(root,width = 420, height = 280)
 
 #キャンバスの位置を指定
 canvas.place(x=0,y=0)
 
-#カメラ用の部分（左上）を塗りつぶし（目印）
-canvas.create_rectangle(200, 100, 420, 280, fill = 'green', stipple = 'gray25')
 
+#お絵かき用のキャンバス（右下）
+canvas2 = tk.Canvas(root,width = 600, height = 400)
 
-#お絵かき用の部分（右下）を塗りつぶし（目印）
-canvas.create_rectangle(500, 300, 900, 600, fill = 'blue', stipple = 'gray25')
+#キャンバスの位置を指定
+canvas2.place(x=570,y=330)
 
 
 root.mainloop()
